4553864a70cbbc7910f2393292daec7c4d1ce303
skergoat <skergoatweb@gmail.com>
2021-05-05 14:19:18 +0700

    test rescatcha

--- index.php
+++ index.php
@@ -3,12 +3,40 @@
     require "vendor/validator.php";
     // if form is sent 
     if(!empty($_POST)) {
-        $validator = new Validator($_POST);
-        $validator->isValid();
-        // validate and send mail
-        if($validator->isValid()) {
-            $validator->send();
-        }
+        
+        // if there is no error in fields 
+        // if captcha is ok
+        define("RECAPTCHA_V3_SECRET_KEY", '6Lff8cYaAAAAAGgMDWHgCyk0LxCMUfZ4P_5MSeg-');
+
+        $name = filter_input(INPUT_POST, $_POST['nom'], FILTER_VALIDATE_EMAIL);
+        $mail = filter_input(INPUT_POST, $_POST['email'], FILTER_SANITIZE_STRING);
+        $password = filter_input(INPUT_POST, $_POST['password'], FILTER_SANITIZE_STRING);
+        $message = filter_input(INPUT_POST, $_POST['message'], FILTER_SANITIZE_STRING);
+        
+        $token = $_POST['token'];
+        $action = $_POST['action'];
+        
+        // call curl to POST request
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_V3_SECRET_KEY, 'response' => $token)));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $arrResponse = json_decode($response, true);
+        
+        // verify the response
+        if($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.5) {
+            return true;
+        } else {
+            $validator = new Validator($_POST);
+            $validator->isValid();
+            // validate and send mail
+            if($validator->isValid()) {
+                $validator->send();
+            }
+        }   
     }
 ?>
 <!doctype html>
